1a4d8f8e58ff2a8f81e076031945931c6273d492
Marko Lovic <markolovic@hotmail.com>
2015-11-15 17:45:13 +0100

    Refactor foromtb

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,8 @@
 require 'active_record'
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :thread_id, presence: true
 
   # make default?
   def description_no_html
