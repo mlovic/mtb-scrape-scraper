f00d295a33333d89046f5ace0c68fdb2f8a4ccf7
Marko Lovic <markolovic@hotmail.com>
2015-12-08 18:42:13 +0100

    Fix date element parsing

--- lib/post.rb
+++ lib/post.rb
@@ -3,6 +3,9 @@ require 'active_record'
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :thread_id, presence: true
+  validates :posted_at, presence: true
+  validates :uri, presence: true
+  validates :last_message_at, presence: true
 
   # make default?
   def description_no_html
