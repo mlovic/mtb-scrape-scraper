f70a3a4d364796795faa8b9aec1610b01984ec9f
Marko Lovic <markolovic@hotmail.com>
2016-09-13 18:03:52 +0200

    Refactor time-related tests to use be_within matcher

--- lib/scraper/date_element_parser.rb
+++ lib/scraper/date_element_parser.rb
@@ -3,7 +3,7 @@ class DateElementParser
   def self.parse(date_element)
     if data_time_attr = date_element.attr('data-time')
       unix_time = data_time_attr.value.to_i
-      return Time.at(unix_time).to_datetime
+      return Time.at(unix_time)
     else
       text = change_month_to_en(date_element.text)
       return Chronic.parse text
--- spec/scraper/date_element_parser_spec.rb
+++ spec/scraper/date_element_parser_spec.rb
@@ -18,15 +18,14 @@ RSpec.describe DateElementParser do
   let(:date_element_3) { build_date_element('post_preview_3.html') }
 
   it 'parses with unix time' do
-    # TODO use be_within
-    expect(DateElementParser.parse(date_element_1).to_time.round).to eq Time.parse('7 Nov 2015 12:59:20 +01:00')
+    expect(DateElementParser.parse(date_element_1)).to be_within(1).of Time.parse('7 Nov 2015 12:59:20 +01:00')
   end
   
   it 'parses without unix time' do
-    expect(DateElementParser.parse(date_element_2)).to eq Time.parse('18 May 2015 12:00')
+    expect(DateElementParser.parse(date_element_2)).to be_within(1).of Time.parse('18 May 2015 12:00')
   end
   it 'parses spanish date' do
-    expect(DateElementParser.parse(date_element_3)).to eq Time.parse('1 Dec 2015 12:00')
+    expect(DateElementParser.parse(date_element_3)).to be_within(1).of Time.parse('1 Dec 2015 12:00')
   end
 
 end
