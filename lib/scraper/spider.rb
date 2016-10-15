158732f56a58aa78ace34b4b643525cca1a335f5
Marko Lovic <markolovic@hotmail.com>
2016-10-15 14:19:15 +0200

    Correct indenting

--- lib/scraper/spider.rb
+++ lib/scraper/spider.rb
@@ -17,26 +17,26 @@ class Spider
   # TODO why mechanize? Switch to Net::HTTP + Nokogiri?
   # TODO no reason for spider to know about handler
   def crawl_async(in_queue, out_queue)
-      @thread = Thread.new do
-                  until in_queue.closed?
-                    (url, handler = in_queue.pop) or Thread.exit
-                    @waiting_for_response = true
-                    logger.debug "Fetching #{url}..."
-                    begin
-                      page = @agent.get(url)
-                    rescue StandardError => e
-                      logger.error "An Error occurred while requesting #{url}:"
-                      logger.error e.message
-                      logger.error e.backtrace
-                      next
-                    end
-                    #check_status(page)
-                    @waiting_for_response = false
-                    #puts "Done fetching #{url}"
-                    out_queue.push [page, handler]
-                    sleep @wait_time # sleep variable amount of time depending on request time?
+    @thread = Thread.new do
+                until in_queue.closed?
+                  (url, handler = in_queue.pop) or Thread.exit
+                  @waiting_for_response = true
+                  logger.debug "Fetching #{url}..."
+                  begin
+                    page = @agent.get(url)
+                  rescue StandardError => e
+                    logger.error "An Error occurred while requesting #{url}:"
+                    logger.error e.message
+                    logger.error e.backtrace
+                    next
                   end
+                  #check_status(page)
+                  @waiting_for_response = false
+                  #puts "Done fetching #{url}"
+                  out_queue.push [page, handler]
+                  sleep @wait_time # sleep variable amount of time depending on request time?
                 end
+              end
   end
 
   def check_status(page)
