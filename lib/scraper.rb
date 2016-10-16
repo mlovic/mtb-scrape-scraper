99931d1da3797b2875479adcde387abce26c77f5
Marko Lovic <markolovic@hotmail.com>
2016-10-16 14:38:09 +0200

    Change scraper api to receive processor as arg

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,57 +1,52 @@
 require 'uri'
 require 'mechanize'
 
-require_relative 'scraper/list_page_handler'
-require_relative 'scraper/post_page_handler'
-require_relative 'scraper/processor'
 require_relative 'scraper/spider'
-require_relative 'scraper/foromtb'
 require_relative 'logging'
 
 class Scraper
-
+  Thread.abort_on_exception = true
   include Logging
 
-  def initialize
+  def initialize(processor)
+    @url_q   = Queue.new
+    @pages_q = Queue.new
+    @processor = processor
   end
 
-  def scrape(num_pages, options = {})
-    offset = options[:start_page] || 1
-    page_range = get_page_range(num_pages, offset) 
-
-    Thread.abort_on_exception = true
-
-    url_q   = Queue.new
-    pages_q = Queue.new
-    
-    pphandler = PostPageHandler.new(url_q)
-    lphandler = ListPageHandler.new(url_q, pphandler)
-    processor = Processor.new(pages_q, pphandler, lphandler)
-
-    # TODO where to put this?
-    root = ForoMtb::FOROMTB_URI
-    page_range.each do |num|
-      url = URI.join(root, "page-#{num}") # TODO where to put this?
-      # Use symbol instead of class reference? 
-      url_q << [url, ListPageHandler]
+  def start(options = {})
+    #offset = options[:start_page] || 1
+    # TODO handle errors in spider thread
+    return false if started?
+    @spider = Spider.new(Mechanize.new) # get rid of hardcoded limit
+    @spider.crawl_async(url_q, pages_q) # Async. Starts thread and returns immediately.
+    # TODO handle potential deadlock error. Sleep and retry?
+    @processor_thread = Thread.new do
+      processor.dispatch(*pages_q.pop) until url_q.empty? && 
+                                             !@spider.waiting_for_response? 
     end
 
-    # TODO handle errors in spider thread
-    spider = Spider.new(Mechanize.new) # get rid of hardcoded limit
-    spider.crawl_async(url_q, pages_q) # Async. Starts thread and returns immediately.
-    # TODO handle potential deadlock error. Sleep and retry?
-    processor.dispatch(*pages_q.pop) until url_q.empty? && 
-                                           !spider.waiting_for_response? 
     logger.debug "Done. Closing queues..."
-    url_q.close # Triggers Spider thread to exit
-    pages_q.close 
+    return true
+    #url_q.close # Triggers Spider thread to exit
+    #pages_q.close 
+  end
+
+  def enq(url, handler)
+    url_q << [url, handler]
+  end
+
+  def stop
+    return false unless started?
+    logger.info "Killing spider and processor threads..."
+    @spider.kill
+    @processor_thread.kill
+  end
+  
+  def started?
+    @processor_thread&.alive?
   end
 
   private
-
-    def get_page_range(num_pages, offset)
-      start_page = offset
-      end_page   = offset + num_pages - 1
-      (start_page..end_page)
-    end
+    attr_accessor :url_q, :pages_q, :processor
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,30 +1,52 @@
-require_relative 'spec_helper'
+#require_relative 'spec_helper'
 require 'scraper'
 
 RSpec.describe Scraper, loads_DB: true do
-  describe '#scrape' do
-    it 'works', slow: true do
+  describe '#start' do
+    let(:spider) { double("spider").as_null_object }
+    let(:processor) { double("processor") }
 
-      VCR.use_cassette 'scrape_first_page' do
+    before { @scraper = Scraper.new(processor) }
+    after  { @scraper.stop }
 
-        # TODO handle thread in test
-        allow(Spider).to receive(:new).and_return(Spider.new(Mechanize.new, time_between_requests: 0))
+    it 'starts instance of spider' do
+      expect(Spider).to receive(:new).and_return spider
+      expect(spider).to receive(:crawl_async)
+      @scraper.start
+    end
 
-        agent = Mechanize.new
-        logger = Logger.new(STDOUT)
-        logger.level = 'INFO'
-        agent.log = logger
-        allow(Mechanize).to receive(:new) {agent}
-
-        scraper = Scraper.new
-        scraper.scrape(1)
-
-        expect(Post.all.size).to eq 20
-        expect(Post.take.description).to_not be_nil
-        expect(Post.take.title).to_not be_nil
-        expect(Post.take.thread_id).to_not be_nil
-        expect(Post.take.posted_at).to_not be_nil
-      end
+    it 'also starts a processing thread' do
+      expect(Thread).to receive(:new).twice
+      @scraper.start
     end
   end
+  
+  describe '#enq' do
+  end
 end
+
+  #describe '#scrape' do
+    #it 'works', slow: true do
+
+      #VCR.use_cassette 'scrape_first_page' do
+
+        ## TODO handle thread in test
+        #allow(Spider).to receive(:new).and_return(Spider.new(Mechanize.new, time_between_requests: 0))
+
+        #agent = Mechanize.new
+        #logger = Logger.new(STDOUT)
+        #logger.level = 'INFO'
+        #agent.log = logger
+        #allow(Mechanize).to receive(:new) {agent}
+
+        #scraper = Scraper.new
+        #scraper.scrape(1)
+
+        #expect(Post.all.size).to eq 20
+        #expect(Post.take.description).to_not be_nil
+        #expect(Post.take.title).to_not be_nil
+        #expect(Post.take.thread_id).to_not be_nil
+        #expect(Post.take.posted_at).to_not be_nil
+      #end
+    #end
+  #end
